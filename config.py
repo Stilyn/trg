09015a1ad327eaced2b864d2e99ea748cc172aa7
stilyn76 <stilyn76@gmail.com>
2023-03-07 17:33:42 +0300

    2 бота и роутер

--- config.py
+++ config.py
@@ -1,23 +1,9 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # -*- coding: utf-8 -*-
-'''Please choose a name for your bot.
-torgi_search_bot
-t.me/trg_search_bot
-'''
 
 # для боевого хоста
 app_dir = '/home/stilyn76/trg/'  # это боевой хост
 whost = '37.230.141.3'  # IP-адрес сервера, на котором запущен бот
 wport = 8443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
 wlisten = '37.230.141.3'  # На некоторых серверах придется указывать такой же IP, что и выше
-
-# это для отладки локально - пока не работает
-#app_dir = '/home/stilyn76/PycharmProjects/trg/' # это для отладки локально
-# whost = '10.0.2.15'  # IP-адрес сервера, на котором запущен бот
-# wport = 8444  # 443, 80, 88 или 8443 (порт должен быть открыт!)
-# wlisten = '10.0.2.15'  # На некоторых серверах придется указывать такой же IP, что и выше
-
-# comment for commit
-token = '6053210739:AAEyEnePJfULSG9hn9nkcHbXIRghZ5fKMug'  # ключ бота
-
